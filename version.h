c71fd54dd7763933123915bbe594eb261e0bac1e
Iwik <30178330+iwikus@users.noreply.github.com>
2026-09-20 12:31:53 +0200

    Add application version header

---
+++ version.h
@@ -0,0 +1,7 @@
+#ifndef APACHETOP_VERSION_H
+#define APACHETOP_VERSION_H
+
+/* Application version, following Semantic Versioning. */
+#define APACHETOP_VERSION "0.1.0"
+
+#endif /* APACHETOP_VERSION_H */
